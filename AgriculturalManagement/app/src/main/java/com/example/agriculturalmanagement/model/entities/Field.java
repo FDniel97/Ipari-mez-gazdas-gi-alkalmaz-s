0085aa5ce014932a2c7a63298442590cc2c69b43
tmblwdchrry <103200423+tmblwdchrry@users.noreply.github.com>
2022-11-10 22:48:42 +0100

    AMSD-18: model.entities.Field: remove object references
    Room does not support object references. Related objects
    will be managed elsewhere.

--- AgriculturalManagement/app/src/main/java/com/example/agriculturalmanagement/model/entities/Field.java
+++ AgriculturalManagement/app/src/main/java/com/example/agriculturalmanagement/model/entities/Field.java
@@ -2,10 +2,7 @@ package com.example.agriculturalmanagement.model.entities;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.ArrayList;
-import java.lang.Exception;
-import java.util.Map;
 
 public class Field{
 
@@ -15,15 +12,13 @@ public class Field{
     private String name;
     private Duration workHours;
     private int precipitationQuantity;
-    private Map<String, CalendarEvent> events;
-    private Crop cropType;
+    private int cropType;
     private double overcastIndex;
     private double lightExposure;// intensity related light exposure, computing from historical overcastIndex
     private LocalDateTime created;
     private double locationLongitude;
     private double locationLatitude;
     private PhysicalAddress physicalAddress;
-    private ComplexArea physicalForm;
 
     public Field() throws Exception{
 
@@ -31,15 +26,12 @@ public class Field{
         name = "";
         workHours = Duration.ZERO;
         precipitationQuantity = 0;
-        events = new HashMap<String, CalendarEvent>();
-        cropType = null;
         overcastIndex = 0.0;
         lightExposure = 1.0;
         created = LocalDateTime.now();
         locationLongitude = 0.0;
         locationLatitude = 0.0;
         physicalAddress = new PhysicalAddress();
-        physicalForm = new ComplexArea();
     }
 
     /* !@brief Constructor for existing entity creation, getting from database.
@@ -48,7 +40,6 @@ public class Field{
     * @param[in] area Size of area of field
     * @param[in] workHours Hours spent with work on the field
     * @param[in] precipitationQuantity The amount of precipitate received
-    * @param[in] events Assigned event to the field
     * @param[in] cropType Type of crop due to homogeneous cultivation on field
     * @param[in] overcastIndex Overcast index for estimating crop development
     * @param[in] lightExposure Light exposure for estimating crop development
@@ -56,24 +47,19 @@ public class Field{
     * @param[in] locationLongitude Horizontal POI
     * @param[in] locationLatitude Vertical POI
     * @param[in] physicalAddress Postal address including parcel address
-    * @param[in] physicalForm Estimated geometric representation of field
     */
     public Field(String name, ComplexArea area, Duration workHours, int precipitationQuantity,
-                 Map<String, CalendarEvent> events, Crop cropType, double overcastIndex, double lightExposure,
+                 int cropType, double overcastIndex, double lightExposure,
                  LocalDateTime created, double locationLongitude, double locationLatitude,
-                 PhysicalAddress physicalAddress, ComplexArea physicalForm) throws Exception {
+                 PhysicalAddress physicalAddress) throws Exception {
 
         id = -1;// has not been used, new entity
 
         setName(name);
 
-        setArea(area);
-
         setWorkHours(workHours);
 
         setPrecipitationQuantity(precipitationQuantity);
-
-        setEvents(events);
 
         setCropType(cropType);
 
@@ -87,8 +73,6 @@ public class Field{
         setLocation(locationLongitude, locationLatitude);
 
         setPhysicalAddress(physicalAddress);
-
-        setPhysicalForm(physicalForm);
     }
 
     /* !@brief Constructor for existing entity creation, getting from database.
@@ -98,7 +82,6 @@ public class Field{
      * @param[in] area Size of area of field
      * @param[in] workHours Hours spent with work on the field
      * @param[in] precipitationQuantity The amount of precipitate received
-     * @param[in] events Assigned event to the field
      * @param[in] cropType Type of crop due to homogeneous cultivation on field
      * @param[in] overcastIndex Overcast index for estimating crop development
      * @param[in] lightExposure Light exposure for estimating crop development
@@ -106,19 +89,17 @@ public class Field{
      * @param[in] locationLongitude Horizontal POI
      * @param[in] locationLatitude Vertical POI
      * @param[in] physicalAddress Postal address including parcel address
-     * @param[in] physicalForm estimated geometric representation of field
      */
     public Field(int id, String name, Duration workHours, int precipitationQuantity,
-                 Map<String, CalendarEvent> events, Crop cropType, double overcastIndex, double lightExposure,
+                 int cropType, double overcastIndex, double lightExposure,
                  LocalDateTime created, double locationLongitude, double locationLatitude,
-                 PhysicalAddress physicalAddress, ComplexArea physicalForm) throws Exception {
+                 PhysicalAddress physicalAddress) {
 
         // assumption of consistent database (skipping data condition tests)
         this.id = id;
         this.name = name;
         this.workHours = workHours;
         this.precipitationQuantity = precipitationQuantity;
-        this.events = events;
         this.cropType = cropType;
         this.overcastIndex = overcastIndex;
         this.lightExposure = lightExposure;
@@ -126,7 +107,6 @@ public class Field{
         this.locationLongitude = locationLongitude;
         this.locationLatitude = locationLatitude;
         this.physicalAddress = physicalAddress;
-        this.physicalForm = physicalForm;
     }
 
     // GETTERS METHODS FOR MEMBERS
@@ -146,11 +126,6 @@ public class Field{
         return name;
     }
 
-    public double getArea() {
-
-        return physicalForm.size();
-    }
-
     public Duration getWorkHours() {
 
         return workHours;
@@ -161,19 +136,7 @@ public class Field{
         return precipitationQuantity;
     }
 
-    public CalendarEvent getEvent(String eventName) throws Exception{
-
-        if(events.containsKey(eventName)) return events.get(eventName);
-
-        throw new Exception("Event not found with provided event name.");
-    }
-
-    public Map<String, CalendarEvent> getEvents() {
-
-        return events;
-    }
-
-    public Crop getCropType() {
+    public int getCropType() {
 
         return cropType;
     }
@@ -208,28 +171,12 @@ public class Field{
         return physicalAddress;
     }
 
-    public ComplexArea getPhysicalForm() {
-
-        return physicalForm;
-    }
-
     // SETTER METHODS FOR MEMBERS
 
     public void setName(String new_name) throws Exception{
 
         if(new_name.isEmpty()) throw new Exception("New name for the field is empty.");
         else this.name = new_name;
-    }
-
-    public void setArea(ComplexArea new_area) throws Exception{
-
-        // concatenating new area chunk with existing ones
-        //  including overlapping and disjunction error handling
-
-        // TODO...
-
-        if(new_area.size() > 0.0) this.physicalForm = new_area;
-        else throw new Exception("Field area is zero.");
     }
 
     /* !@brief Setter that updates work hours strictly in increasing tendency, decrease can not be occurred
@@ -255,49 +202,12 @@ public class Field{
         else throw new Exception("Negative precipitation quantity has provided.");
     }
 
-    /*! @brief Setter that inserts a new event into the event list. Only future events can be placed
-    *          into the list.
-    * @param[in] newEvent Event to be added into the event list
-    */
-    public void setEvent(CalendarEvent newEvent) throws Exception{
-
-        if(newEvent != null){
-
-            // no further handling is needed, internal conditions were handled in type
-            if(LocalDateTime.now().isBefore(newEvent.getTimestamp())) this.events.put(newEvent.getName(), newEvent);
-            else throw new Exception("CalendarEvent was before current time.");
-        }
-        else{
-
-            throw new Exception("CalendarEvent is null.");
-        }
-    }
-
-    /* !@brief Setter that inserts multiple new events into event list. Only future events can be placed
-    *          into the list.
-    *
-    * @param[i] newEvents Events to be added to the event list
-    * */
-    public void setEvents(Map<String, CalendarEvent> newEvents) throws Exception{
-
-        if(events.isEmpty()) throw new Exception("Event list is empty.");
-
-        int sizeOfNewEvents = newEvents.size();
-
-        for(int i = 0; i < sizeOfNewEvents; ++i){
-
-            setEvent(newEvents.get(i));
-        }
-    }
-
     /* !@brief Setter that modifies crop, pragmatic inconsistency would occur in this case
     *
     * @param[in] cropType New type crop
     */
-    public void setCropType(Crop cropType) throws Exception{
-
-        if(cropType != null) this.cropType = cropType;
-        else throw new Exception("Crop type is null.");
+    public void setCropType(int cropType) throws Exception{
+        this.cropType = cropType;
     }
 
     public void setOvercastIndex(double overcastIndex) throws Exception{
@@ -325,19 +235,5 @@ public class Field{
 
         if(physicalAddress != null) this.physicalAddress = physicalAddress;
         else throw new Exception("Physical address is null.");
-    }
-
-    public void setPhysicalForm(ComplexArea physicalForm) throws Exception{
-
-        if(physicalForm != null){
-
-            // validation of physicalFrom is before passing to Field constructor
-            if(physicalForm.size() > 0.0) this.physicalForm = physicalForm;
-            else throw new Exception("Area of field is zero.");
-        }
-        else{
-
-            throw new Exception("Area of field is null.");
-        }
     }
 }
