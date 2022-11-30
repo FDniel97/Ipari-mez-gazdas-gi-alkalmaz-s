b3a371c34835e6a1fff972b0038ecb7fa75e3fed
tmblwdchrry <103200423+tmblwdchrry@users.noreply.github.com>
2022-11-30 23:51:27 +0100

    AMSD-42: model.dao.FieldDao: make `getAll()` return just a list of `Field`

--- AgriculturalManagement/app/src/main/java/com/example/agriculturalmanagement/model/dao/FieldDao.java
+++ AgriculturalManagement/app/src/main/java/com/example/agriculturalmanagement/model/dao/FieldDao.java
@@ -5,13 +5,17 @@ import androidx.room.Dao;
 import androidx.room.Query;
 import androidx.room.Transaction;
 
+import com.example.agriculturalmanagement.model.entities.Field;
 import com.example.agriculturalmanagement.model.entities.FieldWithCropAndEvents;
 
 import java.util.List;
 
 @Dao
 public interface FieldDao {
+    @Query("SELECT * FROM fields")
+    LiveData<List<Field>> getAll();
+
     @Transaction
     @Query("SELECT * FROM fields")
-    LiveData<List<FieldWithCropAndEvents>> getAll();
+    LiveData<List<FieldWithCropAndEvents>> getAllWithCropAndEvents();
 }
