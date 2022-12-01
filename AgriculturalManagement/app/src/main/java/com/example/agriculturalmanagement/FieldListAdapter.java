5e6c2f9a2b2fada95cc79c68877d1a30ab52611d
tmblwdchrry <103200423+tmblwdchrry@users.noreply.github.com>
2022-12-01 23:59:56 +0100

    AMSD-42: open FieldDataFragment when a list item is clicked

--- AgriculturalManagement/app/src/main/java/com/example/agriculturalmanagement/FieldListAdapter.java
+++ AgriculturalManagement/app/src/main/java/com/example/agriculturalmanagement/FieldListAdapter.java
@@ -12,33 +12,45 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.agriculturalmanagement.model.entities.Field;
 
-class FieldListItemViewHolder extends RecyclerView.ViewHolder {
-    private TextView name;
+import java.util.function.Consumer;
 
-    private FieldListItemViewHolder(@NonNull View itemView) {
+class FieldListItemViewHolder extends RecyclerView.ViewHolder {
+    private final TextView name;
+    private Field field;
+
+    private FieldListItemViewHolder(@NonNull View itemView, @NonNull Consumer<Field> clickListener) {
         super(itemView);
         name = itemView.findViewById(R.id.field_name);
+
+        itemView.setOnClickListener(v -> {
+            if (field != null)
+                clickListener.accept(field);
+        });
     }
 
-    static FieldListItemViewHolder create(ViewGroup parent) {
+    static FieldListItemViewHolder create(ViewGroup parent, Consumer<Field> clickListener) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.field_list_item, parent, false);
-        return new FieldListItemViewHolder(view);
+        return new FieldListItemViewHolder(view, clickListener);
     }
 
     void bind(@NonNull Field item) {
         name.setText(item.getName());
+        field = item;
     }
 }
 
 public class FieldListAdapter extends ListAdapter<Field, FieldListItemViewHolder> {
-    public FieldListAdapter() {
+    private final Consumer<Field> clickListener;
+
+    public FieldListAdapter(Consumer<Field> clickListener) {
         super(DIFF_CALLBACK);
+        this.clickListener = clickListener;
     }
 
     @NonNull
     @Override
     public FieldListItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return FieldListItemViewHolder.create(parent);
+        return FieldListItemViewHolder.create(parent, clickListener);
     }
 
     @Override
