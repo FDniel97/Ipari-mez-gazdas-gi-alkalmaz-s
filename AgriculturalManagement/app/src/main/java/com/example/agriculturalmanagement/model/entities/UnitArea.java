48f9279da77ffe5d81ba2708a22c9decc1dd15b4
Akos Arva <arva23@gmail.com>
2022-12-05 13:37:15 +0100

    UnitArea implementation along with tests

--- AgriculturalManagement/app/src/main/java/com/example/agriculturalmanagement/model/entities/UnitArea.java
+++ AgriculturalManagement/app/src/main/java/com/example/agriculturalmanagement/model/entities/UnitArea.java
@@ -1,48 +1,132 @@
 package com.example.agriculturalmanagement.model.entities;
 
 import java.lang.String;
+import java.util.List;
 
-public class UnitArea implements GenArea{
+public class UnitArea extends GenArea{
 
-    private static double longitudeLength;
-    private static double latitudeLength;
+    public static List<UnitArea> unitAreas;
 
-    private double locationLongitude;
-    private double locationLatitude;
+    private static double minAreaLength = 0.0000179664;// above 2m
 
-    public UnitArea(String rawData){
+    private double areaWidth;
+    private double areaHeight;
 
-        // TODO...
+
+    public UnitArea(double areaWidth, double areaHeight, double lon, double lat) throws Exception{
+
+        super();
+
+        coords.add(new LinTreeMap.Pair<GenAreaKey, POI>(new GenAreaKey(315.0), new POI()));
+        coords.add(new LinTreeMap.Pair<GenAreaKey, POI>(new GenAreaKey(45.0), new POI()));
+        coords.add(new LinTreeMap.Pair<GenAreaKey, POI>(new GenAreaKey(135.0), new POI()));
+        coords.add(new LinTreeMap.Pair<GenAreaKey, POI>(new GenAreaKey(225.0), new POI()));
+
+        if(lon < -180.0 || 180.0 <= lon)
+            throw new Exception("Longitudinal location data is out of range (-180.0, 180.0].");
+
+        this.locationLongitude = lon;
+
+        if(lat < -90.0 || 90.0 < lat)
+            throw new Exception("Latitudinal location data is out of range [-90.0, 90.0].");
+
+        this.locationLatitude = lat;
+
+        setAreaWidth(areaWidth);
+        setAreaHeight(areaHeight);
+        addCoord(lon, lat);
+        computeUnitAreaSize();
     }
 
-    public double getLongitude(){
 
-        return locationLongitude;
+    /* !@brief Sets new minimal length of unit area as a lower bound of width and height
+     *         using the equatorial circumference of Earth
+     *
+     * @param[in] newMinAreaLength The new lower threshold in coordinate degrees */
+    public static void setMinAreaLength(double newMinAreaLength) throws Exception{
+
+        newMinAreaLength /= circPol;
+        newMinAreaLength *= 360.0;
+
+        if(newMinAreaLength < 0.0000179663)// equivalent to 2m in horizontal orientation at equator
+            throw new Exception("Minimum area length is less than allowed.");
+
+        minAreaLength = newMinAreaLength;
     }
 
-    public double getLatitude(){
 
-        return locationLatitude;
+    public double getAreaWidth(){
+
+        return areaWidth;
     }
+
+
+    public double getAreaHeight(){
+
+        return areaHeight;
+    }
+
+
+    // suppose that the curvature torsion is not large enough to make visible changes in size of areas
+    //  regarding the simplification of geometry of Earth as a quasi sphere
+    // TODO further improvement could be the use of Haversine formula
+
+    public void setAreaWidth(double newAreaWidth) throws Exception{
+
+
+        newAreaWidth /= circEq;// * Math.abs(Math.sin(Math.toRadians(locationLatitude)));
+        newAreaWidth *= 360.0;
+
+        if(newAreaWidth < minAreaLength)
+            throw new Exception("New value of area width is too small.");
+
+        areaWidth = newAreaWidth;
+    }
+
+    public void setAreaHeight(double newAreaHeight) throws Exception{
+
+        newAreaHeight /= circPol;
+        newAreaHeight *= 360.0;
+
+        if(newAreaHeight < minAreaLength)
+            throw new Exception("New value of area height is too small.");
+
+        areaHeight = newAreaHeight;
+    }
+
+
     @Override
-    public double size() {
+    public void addCoord(double lonVal, double latVal) throws Exception{
 
-        // TODO...
+        // since the four vertices of the unit area are relatively fixed, update the central position solely
+        //  and translate the boundary vertices
 
-        return 0;
+        if(lonVal < -180.0 || 180.0 <= lonVal)
+            throw new Exception("Longitudinal location data is out of range (-180.0, 180.0].");
+
+        this.locationLongitude = lonVal;
+
+        if(latVal <= -90.0 || 90.0 <= latVal)
+            throw new Exception("Latitudinal location data is out of range [-90.0, 90.0].");
+
+        this.locationLatitude = latVal;
+
+        double halfWidth = areaWidth / 2.0;
+        double halfHeight = areaHeight / 2.0;
+
+        coords.setValByInd(0, new POI(315.0, lonVal + halfWidth, latVal - halfHeight, 0));
+        coords.setValByInd(1, new POI(45.0, lonVal + halfWidth, latVal + halfHeight, 0));
+        coords.setValByInd(2, new POI(135.0, lonVal - halfWidth, latVal + halfHeight, 0));
+        coords.setValByInd(3, new POI(225.0, lonVal - halfWidth, latVal - halfHeight, 0));
     }
 
     @Override
-    public void computeUnitArea() {
+    public void computeUnitAreaSize() throws Exception {
 
-        // TODO...
+        areaSize = areaWidth * areaHeight;
+
+        if(areaSize < minimalArea) throw new Exception("Computed area is too small.");
     }
 
-    @Override
-    public String toString(){
 
-        // TODO...
-
-        return "";
-    }
 }
--- AgriculturalManagement/app/src/test/java/com/example/agriculturalmanagement/UnitAreaUnitTests.java
+++ AgriculturalManagement/app/src/test/java/com/example/agriculturalmanagement/UnitAreaUnitTests.java
@@ -1,31 +1,173 @@
 package com.example.agriculturalmanagement;
 
 import static org.junit.Assert.*;
+
+import com.example.agriculturalmanagement.model.UnitArea;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import kotlin.Unit;
 
 public class UnitAreaUnitTests {
 
-    @Test
-    public void constructors_areCorrect(){
+    @Before
+    public void resetStaticField() throws Exception{
 
-        // TODO...
+        UnitArea.setMinAreaLength(2);
+        UnitArea.setMinimalArea(4);
     }
 
     @Test
-    public void getters_areCorrect(){
+    public void constructors_areCorrect() throws Exception{
 
-        // TODO...
+        // empty initialization
+        UnitArea testObject = new UnitArea();
+
+        // explicit initialization with width range error
+        assertEquals("New value of area width is too small.",
+                assertThrows(Exception.class, ()->{
+
+            new UnitArea(0.000001, 100.0, 30.0, 40.0);
+        }).getMessage());
+
+
+        // explicit initialization with height range error
+        assertEquals("New value of area height is too small.",
+                assertThrows(Exception.class, ()->{
+
+            new UnitArea(100.0, 0.0001, 30.0, 40.0);
+        }).getMessage());
+
+
+        // explicit initialization with longitude range error
+        assertEquals("Longitudinal location data is out of range (-180.0, 180.0].",
+                assertThrows(Exception.class, ()->{
+
+            new UnitArea(100.0, 100.0, 300.0, 40.0);
+        }).getMessage());
+
+        assertEquals("Longitudinal location data is out of range (-180.0, 180.0].",
+                assertThrows(Exception.class, ()->{
+
+            new UnitArea(100.0, 100.0, -300.0, 40.0);
+        }).getMessage());
+
+
+        // explicit initialization with latitude range error
+        assertEquals("Latitudinal location data is out of range [-90.0, 90.0].",
+                assertThrows(Exception.class, ()->{
+
+            new UnitArea(100.0, 100.0, 30.0, 400.0);
+
+        }).getMessage());
+
+        assertEquals("Latitudinal location data is out of range [-90.0, 90.0].",
+                assertThrows(Exception.class, ()->{
+
+            new UnitArea(100.0, 100.0, 30.0, -400.0);
+        }).getMessage());
+
+
+        // explicit initialization with size lower bound error
+        UnitArea.setMinimalArea(10);
+        assertEquals("Computed area is too small.",
+                assertThrows(Exception.class, ()->{
+
+            new UnitArea(3, 3, 30.0, 40.0);
+        }).getMessage());
+
+
+        // nominal initialization
+        testObject = new UnitArea(100.0, 100.0, 30.0, 40.0);
+    }
+
+
+    @Test
+    public void GlobalSetters_areCorrect() throws Exception{
+
+        // minAreaLength : lower bound violation
+        assertEquals("Minimum area length is less than allowed.",
+                assertThrows(Exception.class, ()->{
+
+            UnitArea.setMinAreaLength(0.0000001);
+        }).getMessage());
+
+        // minAreaLength : nominal case
+        UnitArea.setMinAreaLength(10.0);
+
+
+        // minimalArea : lower bound violation
+        assertEquals("Minimal area size is less than allowed.",
+                assertThrows(Exception.class, ()->{
+
+            UnitArea.setMinimalArea(0.0000001);
+        }).getMessage());
+
+        // minimalArea : nominal case
+        UnitArea.setMinimalArea(100.0);
     }
 
     @Test
-    public void setters_areCorrect(){
+    public void setters_areCorrect() throws Exception{
 
-        // TODO...
+        UnitArea testObject = new UnitArea();
+
+        // setAreaWidth : lower bound violation
+        assertEquals("New value of area width is too small.",
+                assertThrows(Exception.class, ()->{
+
+            testObject.setAreaWidth(0.00001);
+        }).getMessage());
+
+        // setAreaWidth : nominal case
+        testObject.setAreaWidth(10.0);
+
+
+        // setAreaHeight : lower bound violation
+        assertEquals("New value of area height is too small.",
+                assertThrows(Exception.class, ()->{
+
+            testObject.setAreaHeight(0.00001);
+        }).getMessage());
+
+        // setAreaHeight : nominal case
+        testObject.setAreaHeight(10.0);
+
+
+        // addCoord : out of bounds
+        assertEquals("Longitudinal location data is out of range (-180.0, 180.0].",
+                assertThrows(Exception.class, ()->{
+
+            testObject.addCoord(-300.0, 10.0);
+        }).getMessage());
+
+        // addCoord : out of bounds
+        assertEquals("Latitudinal location data is out of range [-90.0, 90.0].",
+                assertThrows(Exception.class, ()->{
+
+            testObject.addCoord(10.0, 100.0);
+        }).getMessage());
+
+        // addCoord : nominal case
+        testObject.addCoord(10.0, 10.0);
     }
 
     @Test
-    public void serialization_isCorrect(){
+    public void computeUnitArea_isCorrect() throws Exception{
 
-        // TODO...
+        UnitArea testObject = new UnitArea(100.0, 100.0, 30.0, 40.0);
+
+        UnitArea.setMinimalArea(100000);
+        // area is too small (lower bound violation)
+        assertEquals("Computed area is too small.", assertThrows(Exception.class, ()->{
+
+            testObject.computeUnitAreaSize();
+        }).getMessage());
+
+        // nominal case
+        UnitArea.setMinimalArea(10);
+        testObject.setAreaWidth(1000);
+        testObject.computeUnitAreaSize();
     }
 }
