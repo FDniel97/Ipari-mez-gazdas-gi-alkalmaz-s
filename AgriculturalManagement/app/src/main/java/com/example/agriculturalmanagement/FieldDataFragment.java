9064deab9a7e46d92cb1c4402023001fd544c437
tmblwdchrry <103200423+tmblwdchrry@users.noreply.github.com>
2022-12-07 22:47:22 +0100

    AMSD-45: FieldDataFragment: move logic into `onViewCreated()` callback

--- AgriculturalManagement/app/src/main/java/com/example/agriculturalmanagement/FieldDataFragment.java
+++ AgriculturalManagement/app/src/main/java/com/example/agriculturalmanagement/FieldDataFragment.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -15,22 +17,22 @@ public class FieldDataFragment extends Fragment {
     public FieldDataFragment() { }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_field_data, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
         var args = FieldDataFragmentArgs.fromBundle(getArguments());
         Toast.makeText(requireContext(), "id: " + args.getFieldId(), Toast.LENGTH_SHORT).show();
 
         var viewModel = new ViewModelProvider(requireActivity()).get(AppViewModel.class);
-        viewModel.getFieldById(args.getFieldId()).observe(this, field -> {
+        viewModel.getFieldById(args.getFieldId()).observe(getViewLifecycleOwner(), field -> {
             Toast.makeText(requireContext(), "name: " + field.getName(), Toast.LENGTH_SHORT).show();
         });
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_field_data, container, false);
     }
 }
