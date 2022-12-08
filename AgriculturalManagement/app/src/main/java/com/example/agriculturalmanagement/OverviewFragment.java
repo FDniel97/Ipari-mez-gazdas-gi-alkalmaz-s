d2878e2d9ee7b2ed08beca891aca40a10e886d36
tmblwdchrry <103200423+tmblwdchrry@users.noreply.github.com>
2022-12-09 00:21:00 +0100

    AMSD-46: OverviewFragment: add welcome text and field count card

--- AgriculturalManagement/app/src/main/java/com/example/agriculturalmanagement/OverviewFragment.java
+++ AgriculturalManagement/app/src/main/java/com/example/agriculturalmanagement/OverviewFragment.java
@@ -1,64 +1,60 @@
 package com.example.agriculturalmanagement;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link OverviewFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavOptions;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.agriculturalmanagement.databinding.FragmentOverviewBinding;
+import com.example.agriculturalmanagement.model.AppViewModel;
+import com.google.android.material.card.MaterialCardView;
+
 public class OverviewFragment extends Fragment {
+    public MutableLiveData<Integer> fieldCount = new MutableLiveData<>(0);
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public OverviewFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment OverviewFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static OverviewFragment newInstance(String param1, String param2) {
-        OverviewFragment fragment = new OverviewFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public OverviewFragment() { }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_overview, container, false);
+
+        var binding = FragmentOverviewBinding.inflate(inflater, container, false);
+        binding.setViewModel(this);
+        binding.setLifecycleOwner(getViewLifecycleOwner());
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        var navController = Navigation.findNavController(view);
+        var viewModel = new ViewModelProvider(requireActivity()).get(AppViewModel.class);
+
+        viewModel.getAllFields().observe(getViewLifecycleOwner(), newValue -> {
+            fieldCount.setValue(newValue.size());
+        });
+
+        {
+            MaterialCardView c = view.findViewById(R.id.field_count_card);
+            c.setOnClickListener(v -> {
+                // https://stackoverflow.com/questions/71565073/why-does-navigation-not-work-in-the-navigation-drawer-activity-template-with-ver
+                NavOptions navOptions = new NavOptions.Builder()
+                        .setPopUpTo(R.id.overview_dest, false, true)
+                        .setRestoreState(true)
+                        .build();
+                navController.navigate(OverviewFragmentDirections.actionOverviewDestToFieldListDest(), navOptions);
+            });
+        }
     }
 }
