0d643860f75dbadec09d6e07784dd4336e3bbf3f
jakobiadam <jakobiadam@gmail.com>
2022-12-10 17:00:09 +0100

    Add About Us Fragment

--- AgriculturalManagement/app/src/main/java/com/example/agriculturalmanagement/MainActivity.java
+++ AgriculturalManagement/app/src/main/java/com/example/agriculturalmanagement/MainActivity.java
@@ -37,7 +37,9 @@ public class MainActivity extends AppCompatActivity {
                 .Builder(Set.of(
                         R.id.overview_dest,
                         R.id.calendar_dest,
-                        R.id.field_list_dest, R.id.weather_dest))
+                        R.id.field_list_dest,
+                        R.id.weather_dest,
+                        R.id.about_us_dest))
                 .setOpenableLayout(drawerLayout)
                 .build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
