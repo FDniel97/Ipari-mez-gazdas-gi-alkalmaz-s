c3c7dd29b322507ad43d67e92a761905ecbc8746
Akos Arva <arva23@gmail.com>
2022-12-12 22:01:00 +0100

    new conversions are added

--- AgriculturalManagement/app/src/main/java/com/example/agriculturalmanagement/model/Converters.java
+++ AgriculturalManagement/app/src/main/java/com/example/agriculturalmanagement/model/Converters.java
@@ -2,8 +2,20 @@ package com.example.agriculturalmanagement.model;
 
 import androidx.room.TypeConverter;
 
+import com.example.agriculturalmanagement.model.entities.ComplexArea;
+import com.example.agriculturalmanagement.model.entities.GenArea;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Type;
 import java.time.Duration;
 import java.util.Date;
+import java.util.List;
 
 public class Converters {
     @TypeConverter
@@ -24,5 +36,59 @@ public class Converters {
     @TypeConverter
     public static Duration Long2Duration(Long l) {
         return l != null ? Duration.ofNanos(l) : null;
+    }
+
+
+    // TODO
+    // be aware of polymophism
+    // in this version there will be information loss
+    @TypeConverter
+    // using serialization
+    public static String GenArea2String(GenArea g) { return g.toString(); }
+
+    @TypeConverter
+    // using deserialization
+    public static GenArea StringToGenArea(String s) {
+
+        GenArea g = new GenArea();
+        g.fromString(s);
+        return g;
+    }
+
+    @TypeConverter
+    // using serialization
+    public static String GenAreaKey2String(GenAreaKey g) { return GenAreaKey.toString(g); }
+
+    @TypeConverter
+    // using deserialization
+    public static GenAreaKey String2GenAreaKey(String s) { return GenAreaKey.fromString(s); }
+
+    @TypeConverter
+    // using serialization
+    public static String ComplexArea2String(ComplexArea c) { return c.toString(); }
+
+    @TypeConverter
+    // using deserialization
+    public static ComplexArea String2ComplexArea(String s) throws Exception {
+
+        ComplexArea c = new ComplexArea();
+        c.fromString(s);
+
+        return c;
+    }
+
+    @TypeConverter
+    public static String ListGenArea2String(List<GenArea> l) {
+
+        Gson gson = new Gson();
+        return gson.toJson(l);
+    }
+
+    @TypeConverter
+    public static List<GenArea> String2ListGenArea(String s) throws JSONException {
+
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<GenArea> >() { }.getType();
+        return gson.fromJson(s, type);
     }
 }
